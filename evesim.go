7b5bf5e3a2ec7d5a54e94f71c4e0900c9b2f99fd
Aaron S. Joyner <aaron@joyner.ws>
2016-07-25 22:29:57 -0400

    Wrap in a monte carlo simulation, print % outcomes

--- evesim.go
+++ evesim.go
@@ -5,7 +5,13 @@ import (
 	"math/rand"
 )
 
-func main() {
+type result struct {
+	children map[string]int64
+	total    int64
+	maximum  int64
+}
+
+func evesim() map[string]int64 {
 	remainingChildren := map[string]int64{
 		"Eve":      1,
 		"Deborah":  1,
@@ -24,20 +30,62 @@ func main() {
 
 	var winner bool
 	for i := 0; i < ng && !winner; i++ {
-		for geneticLine, children := range remainingChildren {
+		for lineage, children := range remainingChildren {
 			avgChildren := float64(maxGirls) * rand.Float64()
-			remainingChildren[geneticLine] = int64(avgChildren * float64(children))
-			if remainingChildren[geneticLine] >= 1000000000 {
-				fmt.Printf("%s wins with %d children in %d generations.\n", geneticLine, remainingChildren[geneticLine], i)
+			remainingChildren[lineage] = int64(avgChildren * float64(children))
+			if remainingChildren[lineage] >= 1000000000 {
+				//fmt.Printf("%s wins with %d children in %d generations.\n", lineage, remainingChildren[lineage], i)
 				winner = true
 			}
 		}
 	}
+	return remainingChildren
+}
+
+func total(r map[string]int64) int64 {
 	var total int64
-	for _, children := range remainingChildren {
+	for _, children := range r {
 		total += children
 	}
-	for geneticLine, children := range remainingChildren {
-		fmt.Printf("%s: %d (%0.2f%%)\n", geneticLine, children, float64(children)/float64(total)*100)
+	return total
+}
+
+func maximum(r map[string]int64) int64 {
+	var max int64
+	for _, children := range r {
+		if children > max {
+			max = children
+		}
+	}
+	return max
+}
+
+func main() {
+	numSims := 1000
+	results := make([]result, numSims)
+	for i := 0; i < numSims; i++ {
+		r := evesim()
+		results[i] = result{
+			children: r,
+			total:    total(r),
+			maximum:  maximum(r),
+		}
+	}
+
+	for _, threshold := range []float64{0.01, 0.2, 0.5} {
+		var passing int
+		for _, r := range results {
+			for _, children := range r.children {
+				if children == r.maximum {
+					continue // skip the winner
+				}
+				if (float64(children) / float64(r.total)) > threshold {
+					passing++
+					break
+				}
+			}
+		}
+		p := float64(passing) / float64(numSims) * 100
+		fmt.Printf("Odds of a second lineage of >%2.0f%% of the population: %5.2f %%\n", threshold*100.0, p)
 	}
 }
