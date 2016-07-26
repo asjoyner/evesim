2d5348aac88d852217151f6c96ce199a463aa6cc
Aaron S. Joyner <aaron@joyner.ws>
2016-07-25 23:59:50 -0400

    correct the comment describing num children

--- evesim.go
+++ evesim.go
@@ -14,24 +14,24 @@ type result struct {
 
 func evesim() result {
 	var r result
-	initialPopulation := 100 // number of initial lines
-	ng := 4000               // number of generations
-	maxGirls := 2.5          // maximum number of children per generation
+	initialPopulation := 10 // number of initial lines
+	ng := 4000              // number of generations
+	maxGirls := 3.1         // maximum number of children per generation
 
 	r.lines = make([]int64, initialPopulation)
 	for i := range r.lines {
 		r.lines[i] = 1
 	}
 
-	// Simulate as many generation as is takes for one lineage to get to 1
-	// trillion children.  Running too many generations results in overflowing an
+	// Simulate as many generation as is takes for one lineage to get to 8
+	// billion children.  Running too many generations results in overflowing an
 	// int64.  :)
 	var winner bool
 	for i := 0; i < ng && !winner; i++ {
 		for lineage, children := range r.lines {
 			avgChildren := float64(maxGirls) * rand.Float64()
 			r.lines[lineage] = int64(avgChildren * float64(children))
-			if r.lines[lineage] >= 10000000000 {
+			if r.lines[lineage] >= 8000000000 {
 				winner = true
 				r.generations = i
 			}
@@ -75,7 +75,9 @@ func main() {
 	fmt.Printf("Averaged %f generations.\n", g)
 	fmt.Printf("Last generation: %d.\n", maxGeneration)
 
-	for _, threshold := range []float64{0.01, 0.2, 0.5} {
+	// Calculate how many of the simulations had a second mitochondrial linage
+	// had greater than a certain percentage of the children.
+	for _, threshold := range []float64{0.01, 0.1, 0.25} {
 		var passing int
 		for _, r := range results {
 			for _, children := range r.lines {
